e4f5e43342fd9277f63b312ffdc4c72357987e73
dbecerra-O <dbecerraorellana@gmail.com>
2025-06-17 23:53:54 -0500

    update

---
+++ src/pages/ContactPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Contact from '../components/Contact/Contact';
+
+const ContactPage = () => {
+  return (
+    <div className="contact-page">
+      <div style={{ paddingTop: '72px' }}>
+        <Contact />
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
